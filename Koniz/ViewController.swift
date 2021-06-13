f8f73ba2f1f31d3793edf0c8bcf093a7e57ec5c2
MostafaMahmoudMohamed <m.mahmoud@nmowpay.cash>
2021-06-13 16:36:47 +0200

    add inital projectwith ignore file

---
+++ Koniz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Koniz
+//
+//  Created by Mostafa Mahmoud on 6/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
