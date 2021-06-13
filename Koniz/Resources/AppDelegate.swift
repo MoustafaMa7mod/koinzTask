bcb1fdd6ec26988ef3b8bb252ee332748c3b69cf
MostafaMahmoudMohamed <m.mahmoud@nmowpay.cash>
2021-06-13 16:53:14 +0200

    add structure files to project

---
+++ Koniz/Resources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Koniz
+//
+//  Created by Mostafa Mahmoud on 6/13/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
